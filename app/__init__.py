8859b6f3df9dfe18fdc94f8ed9c9dac0bfe49846
Snehi <snehi.joshi.work@gmail.com>
2024-04-13 14:13:16 +0530

    changes

--- app/__init__.py
+++ app/__init__.py
@@ -1,12 +1,16 @@
+import os
 from flask import Flask
 from app.webhook.routes import webhook
 from app.extensions import mongo
+from dotenv import load_dotenv
+
+load_dotenv()
 
 # Creating our flask app
 def create_app():
 
     app = Flask(__name__, template_folder='template/')
-    app.config["MONGO_URI"] = "mongodb://localhost:27017/webhookdb"
+    app.config["MONGO_URI"] = os.getenv('MONGO_URI')
     mongo.init_app(app)
 
     # registering all the blueprints
